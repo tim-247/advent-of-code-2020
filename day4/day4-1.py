f4aa1d72fe78b5ea61a2ce33243a14da6aaf0c36
Tim O'Ryan <tim.oryan@bbc.co.uk>
2020-12-06 07:01:52 +0000

    first attempt

---
+++ day4/day4-1.py
@@ -0,0 +1,46 @@
+with open('day4/input', 'r') as input:
+    inputlines = input.read().splitlines()
+
+
+def lineToDict(line):
+    return dict(p.split(":") for p in line.split())
+
+
+def verifyDict(d):
+    if len(d.keys()) < 7:
+        return False
+    elif len(d.keys()) == 8:
+        return True
+    elif len(d.keys()) == 7:
+        if optional_fields[0] in d.keys():
+            return False
+        else:
+            return True
+    elif len(d.keys()) > 8:
+        raise ValueError("Too many fields in passport")
+
+
+def processLine(line):
+    d = lineToDict(line)
+    return d
+
+
+required_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+optional_fields = ["cid"]
+
+passports = []
+rejected_passports = []
+passport = {}
+
+for line in inputlines:
+    if len(line) > 0:
+        passport.update(processLine(line))
+    else:
+        if verifyDict(passport) is True:
+            passports.append(passport)
+        else:
+            rejected_passports.append(passport)
+        passport = {}
+
+print(max([len(p) for p in rejected_passports]))
+print(len(passports))
