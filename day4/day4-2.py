afb3b9e7f2dff9670d7f2ddf93687c7973bf9d84
Tim O'Ryan <tim.oryan@bbc.co.uk>
2020-12-07 08:46:47 +0000

    4-2 first attempt

---
+++ day4/day4-2.py
@@ -0,0 +1,48 @@
+import re
+
+
+with open('day4/input', 'r') as input:
+    inputlines = input.read().splitlines()
+inputlines.append('')
+
+
+def lineToDict(line):
+    return dict(p.split(":") for p in line.split())
+
+
+def verifyPassport(p):
+    if len(p.keys()) not in [7, 8]:
+        return False
+    elif len(p.keys()) == 8:
+        return True
+    elif len(p.keys()) == 7:
+        if optional_fields[0] in p.keys():
+            return False
+        else:
+            return True
+
+
+def processLine(line):
+    d = lineToDict(line)
+    return d
+
+
+def verifyField(key, value):
+
+required_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+optional_fields = ["cid"]
+
+passports = []
+rejected_passports = []
+passport = {}
+
+for line in inputlines:
+    if len(line) > 0:
+        passport.update(processLine(line))
+    else:
+        if verifyPassport(passport) is True:
+            passports.append(passport)
+        else:
+            rejected_passports.append(passport)
+        passport = {}
+print(len(passports))
